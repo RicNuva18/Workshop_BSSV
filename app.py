f9799dbd37c0eb7a3e88688326088b631e0964f8
jeff <jeffry.hernandez.-nd@disney.com>
2024-08-22 20:54:58 -0500

    Adding major change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Lista para almacenar los elementos
 elements = []
 
-@app.route('/delete_element', methods=['DELETE'])
+@app.route('/v2/delete_element', methods=['DELETE'])
 def delete_element():
     data = request.get_json()
     if 'index' in data:
@@ -19,11 +19,11 @@ def delete_element():
         return jsonify({'error': 'Índice no proporcionado'}), 400
 
 
-@app.route('/get_elements', methods=['GET'])
+@app.route('/v2/get_elements', methods=['GET'])
 def get_elements():
     return jsonify(elements)
 
-@app.route('/add_element', methods=['POST'])
+@app.route('/v2/add_element', methods=['POST'])
 def add_element():
     data = request.get_json()
     if 'element' in data:
@@ -31,6 +31,17 @@ def add_element():
         return jsonify({'message': 'Elemento añadido exitosamente!'}), 201
     else:
         return jsonify({'error': 'Elemento no proporcionado'}), 400
+    
+@app.route('/v2/get_element', methods=['GET'])
+def get_element():
+    index = request.args.get('index', type=int)
+    if index is not None:
+        if 0 <= index < len(elements):
+            return jsonify({'element': elements[index]}), 200
+        else:
+            return jsonify({'error': 'Índice fuera de rango'}), 400
+    else:
+        return jsonify({'error': 'Índice no proporcionado'}), 400
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8000, debug=True)
